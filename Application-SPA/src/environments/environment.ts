fa27e938441c904be8ed7356cc93a00f0a77cb07
hassan kalach <hassan_kalach@hotmail.com>
2020-08-09 12:24:49 +0300

    change local enviroment

--- Application-SPA/src/environments/environment.ts
+++ Application-SPA/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //apiUrl: 'http://localhost:5000/api/'
-  apiUrl: 'https://www.voting-lb.com/api/'
+  apiUrl: 'http://localhost:5000/api/'
+  // apiUrl: 'https://www.voting-lb.com/api/'
 };
 
 /*
